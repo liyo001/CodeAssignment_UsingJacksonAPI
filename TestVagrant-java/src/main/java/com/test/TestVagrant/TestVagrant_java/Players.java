0aed1b8a4289e8e0706e4a094e403908b132e02e
tiwartar <tiwartar@192.168.1.102>
2022-08-01 00:55:37 +0530

    Add new feature in devlop branch.

---
+++ TestVagrant-java/src/main/java/com/test/TestVagrant/TestVagrant_java/Players.java
@@ -0,0 +1,34 @@
+package com.test.TestVagrant.TestVagrant_java;
+
+public class Players {
+
+	private String name;
+    private String country;
+    private String role;
+    private String priceInCrores;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public String getPriceInCrores() {
+        return priceInCrores;
+    }
+    public void setPriceInCrores(String priceInCrores) {
+        this.priceInCrores = priceInCrores;
+    }
+
+}
